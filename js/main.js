7d8f7d3d479e33669a0ec86de7dea3e66a8b0650
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-10-04 22:37:13 -0600

    Se realiza esctructura basica de card recomendaciones

--- js/main.js
+++ js/main.js
@@ -68,3 +68,39 @@ window.addEventListener('load', function(){
         ]
     });
 });
+//Card recomendacion
+window.addEventListener('load', function(){
+    //Elemento donde queremos agregar el carousel y la opcion como queres tener visualmente el carousel
+    new Glider(document.querySelector('#lc3'), {
+        //Cuantos slides se muestran
+        slidesToShow: 1,
+        //Al presionar la flecha cuandos slides avanzar
+        slidesToScroll: 1,
+        //Si se activa que se puede mover con el dedo (celular)
+        draggable: true,
+        //Indicadores (carousel-indicadores)
+        
+        arrows: {
+            prev: '#fl3',
+            next: '#fs3'
+        },
+        responsive: [
+            {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 2,
+            slidesToScroll: 2
+            }
+            },{
+            // screens greater than >= 1024px
+                    breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }
+        ]
+    });
+});
